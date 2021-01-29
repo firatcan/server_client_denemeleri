76c5d49667f368ba253dbee8e3e2d98ffd6a29ff
firatcan <firatcanyildirim7106@gmail.com>
2021-01-29 11:59:32 +0300

    client

---
+++ Client/Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Text;
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            socket.Connect(IPAddress.Parse("127.0.0.1"),600);//gönderilecek veri
+            byte[] buffer = Encoding.ASCII.GetBytes(Console.ReadLine());
+            socket.Send(buffer);
+            socket.Close();
+            Console.Read();
+        }
+    }
+}
